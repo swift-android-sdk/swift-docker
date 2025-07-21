7af65b123f3d12f94d73f098111ae418d0b886d6
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-07-21 17:50:20 -0400

    Remove flag -DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON

--- swift-ci/sdks/android/scripts/build.sh
+++ swift-ci/sdks/android/scripts/build.sh
@@ -462,8 +462,8 @@ for arch in $archs; do
             --llvm-ninja-targets-for-cross-compile-hosts=help \
             --libdispatch-cmake-options=-DCMAKE_SHARED_LINKER_FLAGS= \
             --foundation-cmake-options=-DCMAKE_SHARED_LINKER_FLAGS= \
-            --extra-cmake-options='-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON' \
             --cross-compile-append-host-target-to-destdir=False 
+
 
             # --extra-cmake-options='-DCMAKE_EXTRA_LINK_FLAGS="-Wl,-z,max-page-size=16384"'
         # need to remove symlink that gets created in the NDK to the previous arch's build
