e7d18605b665073e6a00f34b620284e5f28d9f8d
Marc Prud'hommeaux <mwp1@cornell.edu>
2025-07-21 15:53:35 -0400

    Push to trigger CI

--- swift-ci/sdks/android/scripts/build.sh
+++ swift-ci/sdks/android/scripts/build.sh
@@ -462,6 +462,7 @@ for arch in $archs; do
             --llvm-ninja-targets-for-cross-compile-hosts=help \
             --extra-cmake-options='-DANDROID_SUPPORT_FLEXIBLE_PAGE_SIZES=ON' \
             --cross-compile-append-host-target-to-destdir=False 
+
             # --extra-cmake-options='-DCMAKE_EXTRA_LINK_FLAGS="-Wl,-z,max-page-size=16384"'
         # need to remove symlink that gets created in the NDK to the previous arch's build
         # or else we get errors like:
