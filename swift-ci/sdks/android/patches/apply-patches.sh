053d2b15636ecef3a8bdfbe8a6472bb7298ce2b4
Finagolfin <finagolfin@tuta.io>
2025-08-14 20:35:26 +0530

    Turn the last trunk-only patch into a perl substitution instead and reorganize the patches a bit

--- swift-ci/sdks/android/patches/apply-patches.sh
+++ swift-ci/sdks/android/patches/apply-patches.sh
@@ -2,21 +2,15 @@
 
 patches_dir=$(dirname $(realpath -- "${BASH_SOURCE[0]}"))
 cd ${1}
+git apply -v -C1 ${patches_dir}/swift-android.patch
 
-case "${BUILD_SCHEME}" in
-    swift-*-branch)
-        git apply -v -C1 ${patches_dir}/swift-android-devel.patch
-        git apply -v -C1 ${patches_dir}/swift-android.patch
-        ;;
-    development)
-        git apply -v -C1 ${patches_dir}/swift-android.patch
-        git apply -v -C1 ${patches_dir}/swift-android-trunk.patch
-        ;;
-    *)
-        echo "$0: invalid BUILD_SCHEME=${BUILD_SCHEME}"
-        exit 1
-        ;;
-esac
+if [[ "${BUILD_SCHEME}" == "swift-6.2-branch" ]]; then
+    git apply -v -C1 ${patches_dir}/swift-android-devel.patch
+else
+    # This `git grep` invocation in a trunk test fails in our Docker for some
+    # reason, so just turn it into a plain `grep` again.
+    perl -pi -e 's:"git",:#:' swift/test/Misc/verify-swift-feature-testing.test-sh
+fi
 
 # disable backtrace() for Android (needs either API33+ or libandroid-execinfo, or to manually add in backtrace backport)
 perl -pi -e 's;os\(Android\);os\(AndroidDISABLED\);g' swift-testing/Sources/Testing/SourceAttribution/Backtrace.swift
